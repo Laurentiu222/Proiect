e128c1ea02a951e7a15386ab870047bc39657a65
andreisva07 <m.andreysva@gmail.com>
2023-04-25 09:15:43 +0300

    FlightApllication : -added applogic, data abstraction, data model, data access

---
+++ FLightApplication/FLightApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FlightManagement.DataModel;
+using FlightManagement.DataAccess;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace FlightManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext) : base(dbContext) 
+        {
+
+        }
+        public DbSet<FlightManagement.DataModel.Booking> Booking { get; set; } = default!;
+    }
+}
